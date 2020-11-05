85947b10604adcd9bfe9cda939d39df9984b66d3
Børge Lundsaunet <blund@blund.site>
2020-11-05 10:09:30 +0100

    Finish task 2

--- project_tasks/src/main/scala/Account.scala
+++ project_tasks/src/main/scala/Account.scala
@@ -1,4 +1,5 @@
 import exceptions._
+import scala.Either
 
 class Account(val bank: Bank, initialBalance: Double) {
 
@@ -9,9 +10,35 @@ class Account(val bank: Bank, initialBalance: Double) {
   // TODO
   // for project task 1.2: implement functions
   // for project task 1.3: change return type and update function bodies
-  def withdraw(amount: Double): Unit = ???
-  def deposit(amount: Double): Unit = ???
-  def getBalanceAmount: Double = ???
+  def withdraw(amount: Double): Either[Double, String] = {
+      balance.synchronized {
+        if(amount < 0) {
+            Right("Cannot withdraw negative value")
+        }
+        else if(balance.amount < amount) {
+            Right("Insufficient funds")
+        }
+        else {
+            balance.amount -= amount
+            Left(balance.amount)
+        }
+      }
+  }
+
+  def deposit(amount: Double): Either[Double, String] = {
+      balance.synchronized {
+         if(amount < 0) {
+            Right("cannot deposit negative value")
+         } else {
+            balance.amount += amount
+            Left(balance.amount)
+         }
+      }
+  }
+
+  def getBalanceAmount: Double = balance.synchronized {
+      balance.amount
+  }
 
   def transferTo(account: Account, amount: Double) = {
     bank addTransactionToQueue (this, account, amount)
--- project_tasks/src/main/scala/Bank.scala
+++ project_tasks/src/main/scala/Bank.scala
@@ -3,14 +3,31 @@ class Bank(val allowedAttempts: Integer = 3) {
   private val transactionsQueue: TransactionQueue = new TransactionQueue()
   private val processedTransactions: TransactionQueue = new TransactionQueue()
 
-  def addTransactionToQueue(from: Account, to: Account, amount: Double): Unit =
-    ???
+  def addTransactionToQueue(from: Account, to: Account, amount: Double): Unit = {
+      transactionsQueue.push(
+        new Transaction(transactionsQueue, processedTransactions, from, to, amount, allowedAttempts)
+      )
+      val thread = new Thread {
+          processTransactions
+      }
+      thread.run()
+  }
   // TODO
   // project task 2
   // create a new transaction object and put it in the queue
   // spawn a thread that calls processTransactions
 
-  private def processTransactions: Unit = ???
+  private def processTransactions: Unit = {
+      var transaction = transactionsQueue.pop
+      if(transaction.status == TransactionStatus.PENDING) {
+          transactionsQueue.push(transaction)
+          processTransactions
+      }
+      else {
+          processedTransactions.push(transaction)
+      }
+  }
+
   // TOO
   // project task 2
   // Function that pops a transaction from the queue
