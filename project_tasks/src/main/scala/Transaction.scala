926dfdc2809a29bbd9c8ee30ae6acff84392c08e
Børge Lundsaunet <blund@blund.site>
2020-11-05 10:01:00 +0100

    Finish first task

--- project_tasks/src/main/scala/Transaction.scala
+++ project_tasks/src/main/scala/Transaction.scala
@@ -1,5 +1,5 @@
 import exceptions._
-import scala.collection.mutable
+import scala.collection.mutable.Queue
 
 object TransactionStatus extends Enumeration {
   val SUCCESS, PENDING, FAILED = Value
@@ -10,21 +10,42 @@ class TransactionQueue {
   // TODO
   // project task 1.1
   // Add datastructure to contain the transactions
+  private val queue = new Queue[Transaction]()
 
   // Remove and return the first element from the queue
-  def pop: Transaction = ???
+  def pop: Transaction = {
+      queue.synchronized {
+          queue.dequeue
+      }
+  }
 
   // Return whether the queue is empty
-  def isEmpty: Boolean = ???
+  def isEmpty: Boolean = {
+      queue.synchronized {
+          queue.isEmpty
+      }
+  }
 
   // Add new element to the back of the queue
-  def push(t: Transaction): Unit = ???
+  def push(t: Transaction): Unit = {
+      queue.synchronized {
+          queue += t
+      }
+  }
 
   // Return the first element from the queue without removing it
-  def peek: Transaction = ???
+  def peek: Transaction = {
+      queue.synchronized {
+          queue.head
+      }
+  }
 
   // Return an iterator to allow you to iterate over the queue
-  def iterator: Iterator[Transaction] = ???
+  def iterator: Iterator[Transaction] = {
+      queue.synchronized {
+          queue.iterator
+      }
+  }
 }
 
 class Transaction(
